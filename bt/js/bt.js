85fbcc2385be0de7110246ace5979de34ef2563a
Paulo <paulojr2@gmail.com>
2019-07-05 11:28:08 -0300

    1.3

--- bt/js/bt.js
+++ bt/js/bt.js
@@ -1,56 +1,34 @@
 (function() {
 
-    const arrOpts = [
-        {}
-    ];
-    
-  
 
-        const bttn = document.querySelector('button.particles-button');
-        const bttnBack = document.querySelector('button.action');
-        
-        let particlesOpts = arrOpts[0];
-        particlesOpts.complete = () => {
-            if ( !buttonVisible ) {
-                anime.remove(bttnBack);
-                anime({
-                    targets: bttnBack,
-                    duration: 300,
-                    easing: 'easeOutQuint',
-                    opacity: 1,
-                    scale: [0,1]
-                });
-                bttnBack.style.pointerEvents = 'auto';
-            }
-        };
-        const particles = new Particles(bttn, particlesOpts);
+    
+    const items = document.querySelectorAll('.btdiv');
+    items.forEach((el, pos) => {
+        const bttn = el.querySelector('button.particles-button');
+ 
+        const particles = new Particles(bttn);
         
         let buttonVisible = true;
         bttn.addEventListener('click', () => {
             if ( !particles.isAnimating() && buttonVisible ) {
                 particles.disintegrate();
                 buttonVisible = !buttonVisible;
+      
+                setTimeout(bla,5000);
             }
         });
-        bttnBack.addEventListener('click', () => {
-            if ( !particles.isAnimating() && !buttonVisible ) {
-                anime.remove(bttnBack);
-                anime({
-                    targets: bttnBack,
-                    duration: 300,
-                    easing: 'easeOutQuint',
-                    opacity: 0,
-                    scale: 0
-                });
-                bttnBack.style.pointerEvents = 'none';
+        function bla() {
+            if ( !buttonVisible ) {
 
                 particles.integrate({
-                    duration: 800,
+                    duration: 1000,
                     easing: 'easeOutSine'
                 });
+  
                 buttonVisible = !buttonVisible;
-            }
-        });
 
+            }
+        };
+    });
 
 })();
